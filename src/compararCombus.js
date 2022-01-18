d410993d3fb1542ce10eb75fc979ad0a1b9cbc9a
Gabriel Fernandes <gabgui2001@gmail.com>
2022-01-18 14:03:38 -0300

    .

--- src/compararCombus.js
+++ src/compararCombus.js
@@ -9,13 +9,14 @@ var Funcomparar =function compararFun(){
 
     infos1.push(prompt("Digite o nome de um dos combustiveis: "))
     infos1.push(prompt("Digite quantos Km/L ele faz: "))
+    infos1.push(prompt("Digite o preço do combustivel: "))
     infos2.push(prompt("Digite o nome do segundo combustivel: "))
     infos2.push(prompt("Digite quantos Km/L ele faz: "))
+    infos2.push(prompt("Digite o preço do combustivel: "))
 
-    let resul1 = (infos1[1]/infos1[2])
-    let resul2 = (infos2[1]/infos2[2])
-
-    console.log(`No ${infos1[1]} voce terar um gasto de ${resul1} por Km \nNo ${infos2[1]} voce terar um gasto de ${resul2} por Km `)
+    let resul1 = Number((infos1[2]/infos1[1]))
+    let resul2 = Number((infos2[2]/infos2[1]))
+    console.log(`No ${infos1[0]} voce terar um gasto de ${resul1} por Km \nNo ${infos2[0]} voce terar um gasto de ${resul2} por Km `)
     console.log("deseja fazer outra conta ? Se sim digite 1 se não digite 2 para voltar ao menu")
     let restart = prompt()
     if(restart == 1){
