42d71d3548e88fa3c71b13a2ea1aaaf99db0ec4b
Gabriel Fernandes <gabgui2001@gmail.com>
2022-01-23 19:12:06 -0300

    add objects

--- src/compararCombus.js
+++ src/compararCombus.js
@@ -1,22 +1,32 @@
 const prompt = require('prompt-sync')()
 //const choice = require('./choice')
 
-
 var Funcomparar =function compararFun(){
     // Gas1, Km/l1, preco1
-    let infos1= []
-    let infos2= []
 
-    infos1.push(prompt("Digite o nome de um dos combustiveis: "))
-    infos1.push(prompt("Digite quantos Km/L ele faz: "))
-    infos1.push(prompt("Digite o preço do combustivel: "))
-    infos2.push(prompt("Digite o nome do segundo combustivel: "))
-    infos2.push(prompt("Digite quantos Km/L ele faz: "))
-    infos2.push(prompt("Digite o preço do combustivel: "))
-
-    let resul1 = Number((infos1[2]/infos1[1]))
-    let resul2 = Number((infos2[2]/infos2[1]))
-    console.log(`No ${infos1[0]} voce terar um gasto de ${resul1.toFixed(2)} por Km \nNo ${infos2[0]} voce terar um gasto de ${resul2.toFixed(2)} por Km `)
+    const gas1 ={
+        nome:"Gasolina",
+        kmL: 10.5 ,
+        preco: 6.68,
+    }
+    
+    const gas2 ={
+        nome:"Etanol",
+        kmL: 7.5 ,
+        preco: 5.98,
+    }
+    
+    // infos1.push(prompt("Digite o nome de um dos combustiveis: "))
+    // infos1.push(prompt("Digite quantos Km/L ele faz: "))
+    // infos1.push(prompt("Digite o preço do combustivel: "))
+    // infos2.push(prompt("Digite o nome do segundo combustivel: "))
+    // infos2.push(prompt("Digite quantos Km/L ele faz: "))
+    // infos2.push(prompt("Digite o preço do combustivel: "))
+    
+    let resul1 = Number((gas1.preco/gas1.kmL))
+    let resul2 = Number((gas2.preco/gas2.kmL))
+    
+    console.log(`No ${gas1.nome} voce terar um gasto de R$ ${resul1.toFixed(2)} por Km \nNo ${gas2.nome} voce terar um gasto de R$ ${resul2.toFixed(2)} por Km `)
     console.log("deseja fazer outra conta ? Se sim digite 1 se não digite 2 para voltar ao menu")
     let restart = prompt()
     if(restart == 1){
@@ -28,4 +38,5 @@ var Funcomparar =function compararFun(){
 }
 //compararFun()
 
+Funcomparar()
 module.exports = Funcomparar
